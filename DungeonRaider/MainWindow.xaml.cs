86918af3a3b7643b3782cfc7e123cb4ff40f34f1
HeFahr <hendrikch@gmx.de>
2016-12-03 13:50:59 +0100

    Initial Commit

---
+++ DungeonRaider/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace DungeonRaider
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DungeonButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
+    }
+}
