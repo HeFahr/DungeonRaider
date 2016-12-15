01bca00803732495c18070e1486c7c7c68ee5309
HeFahr <hendrikch@gmx.de>
2016-12-15 19:25:51 +0100

    - added equipment folder and subfolders for weapons and armor - added maces and axes - added character item placeholders

--- DungeonRaider.ItemBuilder/ItemBuilder.cs
+++ DungeonRaider.ItemBuilder/ItemBuilder.cs
@@ -1,6 +1,72 @@
-﻿namespace DungeonRaider.ItemBuilder
+﻿using DungeonRaider.ItemBuilder.Types;
+using DungeonRaider.Types;
+
+namespace DungeonRaider.ItemBuilder
 {
     public class ItemBuilder
     {
+        public Item BuildWeapon(ItemDegree degree)
+        {
+            Item item = new Item(RarenessLevel.Common, degree);
+            
+            return item;
+        }
+
+        public Item BuildHelmet(ItemDegree degree)
+        {
+            Item item = new Item(RarenessLevel.Common, degree);
+
+            return item;
+        }
+
+        public Item BuildShoulders(ItemDegree degree)
+        {
+            Item item = new Item(RarenessLevel.Common, degree);
+
+            return item;
+        }
+
+        public Item BuildChest(ItemDegree degree)
+        {
+            Item item = new Item(RarenessLevel.Common, degree);
+
+            return item;
+        }
+
+        public Item BuildBracers(ItemDegree degree)
+        {
+            Item item = new Item(RarenessLevel.Common, degree);
+
+            return item;
+        }
+
+        public Item BuildGauntlets(ItemDegree degree)
+        {
+            Item item = new Item(RarenessLevel.Common, degree);
+
+            return item;
+        }
+
+        public Item BuildBelt(ItemDegree degree)
+        {
+            Item item = new Item(RarenessLevel.Common, degree);
+
+            return item;
+        }
+
+        public Item BuildPants(ItemDegree degree)
+        {
+            Item item = new Item(RarenessLevel.Common, degree);
+
+            return item;
+        }
+
+        public Item BuildBoots(ItemDegree degree)
+        {
+            Item item = new Item(RarenessLevel.Common, degree);
+
+            return item;
+        }
+
     }
 }
